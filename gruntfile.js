374caf02ade7ddbd9db145f42f7c57a29a145bfc
Kira Miller <kira.miller2011@gmail.com>
2019-11-25 15:12:37 -0800

    added automation

--- gruntfile.js
+++ gruntfile.js
@@ -1,9 +1,44 @@
 // The wrapper function:
 module.exports = function(grunt) {
 
+	const sass = require('node-sass');
+
 	// Project and task configuration:
 	grunt.initConfig({
-		pkg: grunt.file.readJSON('package.json')
+		pkg: grunt.file.readJSON('package.json'),
+
+		/**
+		* Grunt Sass
+		* Compile Sass to CSS using node-sass
+		* https://www.npmjs.com/package/grunt-sass
+		*/
+		sass: {
+			options: {
+				implementation: sass,
+				sourceMap: false
+			},
+			dist: {
+				files: {
+					'css/styles.css' : 'assets/scss/styles.scss'
+				}
+			}
+		},
+
+		/**
+		* Grunt Contrib Watch
+		* Monitor files and execute tasks
+		* https://www.npmjs.com/package/grunt-contrib-watch
+		*/
+		watch: {
+			sass: {
+				files: [
+					'assets/scss/*.scss'
+				],
+				tasks: [
+					'sass'
+				]
+			}
+		}
 
 	});
 
@@ -11,6 +46,6 @@ module.exports = function(grunt) {
 	require('load-grunt-tasks')(grunt);
 
 	// Custom tasks:
-	grunt.registerTask('default', []);
-	
+	grunt.registerTask('default', ['watch']);
+
 };
